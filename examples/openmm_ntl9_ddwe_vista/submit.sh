85ad48e0510496a82e87cbb67aecd86dda7425fa
NikJur <93187448+NikJur@users.noreply.github.com>
2026-03-23 15:20:39 +0000

    Update SLURM script for DDMD test configuration

--- examples/openmm_ntl9_ddwe_vista/submit.sh
+++ examples/openmm_ntl9_ddwe_vista/submit.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-#SBATCH -J idev15
-#SBATCH -o idev15.o%j
+#SBATCH -J DDMD_test
+#SBATCH -o DDMD_test.o%j
 #SBATCH -N 2
 #SBATCH --ntasks-per-node=1
 #SBATCH -p ghtest
 #SBATCH -t 00:30:00
-#SBATCH -A bddur53
-#SBATCH --mail-type=END
-#SBATCH --mail-user=nikolai.juraschko@bioch.ox.ac.uk
-
+#SBATCH -A <project_code>
 
 #------------------------------------------------------
-# Source the bashrc to add conda
-source /nobackup/projects/bddur53/DDMD_WE_GH_2/conda_folder/miniconda/etc/profile.d/conda.sh
-#source ~/.bashrc
+# Source conda
+source /nobackup/projects/<project_code>/<user_name>/aarch64/miniconda/etc/profile.d/conda.sh
+# Alternatively, add it to your bashrc and source ~/.bashrc
 
 # Load the required modules
-#ml gcc/14.2.0 cuda/12.5 hdf5
 module load gcc/14.2
 module load cuda/12.5.1
 module load hdf5
@@ -25,15 +21,13 @@ module load hdf5
 unset SLURM_CPUS_PER_TASK
 unset SLURM_TRES_PER_TASK
 
-conda activate deepdrivewe_VISTA_2
+conda activate deepdrivewe
 
 # Change to working directory
-#cd /scratch/08288/abrace/projects/ddwe/src/deepdrivewe
-cd /nobackup/projects/bddur53/DDMD_WE_GH_2/TRY_2/deepdrive_we-BEDE
+cd /nobackup/projects/<project_code>/<user_name>/aarch64/deepdrive_we-BEDE
 
 # Get the config file for this example
-#CONFIG_FILE=/scratch/08288/abrace/projects/ddwe/src/deepdrivewe/examples/openmm_ntl9_ddwe_vista/config.yaml
-CONFIG_FILE=/nobackup/projects/bddur53/DDMD_WE_GH_2/TRY_2/deepdrive_we-BEDE/examples/openmm_ntl9_ddwe_vista/config.yaml
+CONFIG_FILE=/nobackup/projects/<project_code>/<user_name>/aarch64/deepdrive_we-BEDE/examples/openmm_ntl9_ddwe_vista/config.yaml
 
 # Start a background resource monitor that logs every 10 seconds
 (while true; do date; nvidia-smi; free -h; sleep 10; done) > resource_usage.log &
