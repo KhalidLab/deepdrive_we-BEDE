ca576847161ebd04c589b70c6bdc739696e7d5bc
NikJur <nikolai.juraschko@rfi.ac.uk>
2026-03-24 10:48:51 +0000

    will make next merge from README branch easier

--- examples/openmm_ntl9_ddwe_vista/submit.sh
+++ examples/openmm_ntl9_ddwe_vista/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -J idev15
-#SBATCH -o idev15.o%j
+#SBATCH -J DDMD_test_2
+#SBATCH -o DDMD_test_2.o%j
 #SBATCH -N 2
 #SBATCH --ntasks-per-node=1
 #SBATCH -p ghtest
@@ -11,12 +11,11 @@
 
 
 #------------------------------------------------------
-# Source the bashrc to add conda
-source /nobackup/projects/bddur53/DDMD_WE_GH_2/conda_folder/miniconda/etc/profile.d/conda.sh
-#source ~/.bashrc
+# Source conda
+source /nobackup/projects/bddur53/nikjur/aarch64/miniconda/etc/profile.d/conda.sh
+# Alternatively add it to your bashrc and source ~/.bashrc
 
 # Load the required modules
-#ml gcc/14.2.0 cuda/12.5 hdf5
 module load gcc/14.2
 module load cuda/12.5.1
 module load hdf5
@@ -25,19 +24,21 @@ module load hdf5
 unset SLURM_CPUS_PER_TASK
 unset SLURM_TRES_PER_TASK
 
-conda activate deepdrivewe_VISTA_2
+conda activate deepdrivewe
 
 # Change to working directory
-#cd /scratch/08288/abrace/projects/ddwe/src/deepdrivewe
-cd /nobackup/projects/bddur53/DDMD_WE_GH_2/TRY_2/deepdrive_we-BEDE
+cd /nobackup/projects/bddur53/nikjur/aarch64/deepdrive_we-BEDE
 
 # Get the config file for this example
-#CONFIG_FILE=/scratch/08288/abrace/projects/ddwe/src/deepdrivewe/examples/openmm_ntl9_ddwe_vista/config.yaml
-CONFIG_FILE=/nobackup/projects/bddur53/DDMD_WE_GH_2/TRY_2/deepdrive_we-BEDE/examples/openmm_ntl9_ddwe_vista/config.yaml
+CONFIG_FILE=/nobackup/projects/bddur53/nikjur/aarch64/deepdrive_we-BEDE/examples/openmm_ntl9_ddwe_vista/config.yaml
 
 # Start a background resource monitor that logs every 10 seconds
 (while true; do date; nvidia-smi; free -h; sleep 10; done) > resource_usage.log &
 MONITOR_PID=$!
+
+# If CUDA still throwing error with conda version mismatch:
+# export CUDA_HOME=$CUDA_INSTALL_PATH
+# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 # Run the example
 python -m deepdrivewe.examples.openmm_ntl9_ddwe.main --config $CONFIG_FILE
